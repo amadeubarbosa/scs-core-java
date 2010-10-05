52efff2c8a46d23d9b57949499e02148cab18b37
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2010-10-05 16:28:31 +0000

    [SCS-188] Atualizando regras de compilação do SCS Core Java * Incluindo descritores do Maven referentes ao Jacorb (visto que o projeto Jacorb não fornece seus próprios descritores). * Comentei seções referentes ao Junit porque não temos testes no scs.core ainda. * Removi suporte ao ANT (build.xml e script maven-repository-installer.sh).
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/java/trunk@110927 ae0415b3-e90b-0410-900d-d0be9363c56b

--- maven-repository-installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# change here to upgrade maven version information!
-VERSION=1.2-SNAPSHOT
-
-ant jar-core
-mvn install:install-file -DgroupId=tecgraf.scs -DartifactId=scs-core -Dversion=${VERSION} -Dpackaging=jar -Dfile=libs/scs.jar
-mvn install:install-file -DgroupId=tecgraf.scs -DartifactId=scs-idl-jacorb -Dversion=${VERSION} -Dpackaging=jar -Dfile=libs/scsidl.jar
---
+++ scripts/jacorb-maven-installer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+VERSION=2.3.0
+
+mvn install:install-file -DgroupId=org.jacorb \
+    -DartifactId=jacorb \
+    -Dversion=$VERSION \
+    -Dpackaging=jar \
+    -Dfile=libs/jacorb-$VERSION.jar \
+    -DpomFile=scripts/jacorb-pom.xml
+
+mvn install:install-file -DgroupId=org.jacorb \
+    -DartifactId=jacorb-idl-compiler \
+    -Dversion=$VERSION \
+    -Dpackaging=jar \
+    -Dfile=libs/jacorb-idl-$VERSION.jar \
+    -DpomFile=scripts/jacorb-idl-compiler-pom.xml
+
