df0070990bf80e5c875390b598912827ace7aad7
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2010-11-09 17:37:38 +0000

    [SCS-211] Ajustes no nome de versão e URLs.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/java/branches/SCS_CORE_JAVA_v1_02_2010_11_08@112319 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demos/helloworld/bin/rundemo.bat
+++ demos/helloworld/bin/rundemo.bat
@@ -9,6 +9,6 @@ IF NOT DEFINED SCS_HOME SET SCS_HOME=%DEFAULT_SCS_HOME%
 ECHO   [ INFO ] 
 ECHO   Using the following PATH: %PATH%
 ECHO   [ Launching the demo: %1 ... ]
-SET CLASSPATH="%SCS_HOME%\avalon-framework-4.1.5.jar;%SCS_HOME%\jacorb-2.3.0.jar;%SCS_HOME%\logkit-1.2.jar;%SCS_HOME%\scs-idl-jacorb-1.2-SNAPSHOT.jar;%SCS_HOME%\scs-core-1.2-SNAPSHOT.jar;%SCS_HOME%\scs-demos-helloworld-1.2-SNAPSHOT.jar"
+SET CLASSPATH="%SCS_HOME%\avalon-framework-4.1.5.jar;%SCS_HOME%\jacorb-2.3.0.jar;%SCS_HOME%\logkit-1.2.jar;%SCS_HOME%\scs-idl-jacorb-1.2.0.jar;%SCS_HOME%\scs-core-1.2.0.jar;%SCS_HOME%\scs-demos-helloworld-1.2.0.jar"
 java -Djava.library.path=%SCS_HOME% -Djava.endorsed.dirs=%SCS_HOME% -Djacorb.home=%SCS_HOME% -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton %*
 ENDLOCAL
