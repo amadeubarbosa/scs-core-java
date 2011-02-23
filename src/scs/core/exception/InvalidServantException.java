a90a42715582316c4791d155021fc04f71136310
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2011-02-23 15:11:07 +0000

    [SCS-209, SCS-221] Novo javadoc para o pacote core.exception, em inglês.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/scs/core/java/trunk@115737 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/scs/core/exception/InvalidServantException.java
+++ src/scs/core/exception/InvalidServantException.java
@@ -2,15 +2,33 @@ package scs.core.exception;
 
 import org.omg.CORBA.UserException;
 
+/**
+ * This exception signalizes that an invalid servant has been received as a
+ * parameter.
+ * 
+ */
 public class InvalidServantException extends SCSException {
 
+  /**
+   * Default constructor.
+   */
   public InvalidServantException() {
   }
 
+  /**
+   * Constructor that accepts an UserException as a cause.
+   * 
+   * @param cause The UserException object.
+   */
   public InvalidServantException(UserException cause) {
     super(cause);
   }
 
+  /**
+   * Constructor that accepts a Throwable as a cause.
+   * 
+   * @param cause The Throwable object.
+   */
   public InvalidServantException(Throwable cause) {
     super(cause);
   }
--- src/scs/core/exception/SCSException.java
+++ src/scs/core/exception/SCSException.java
@@ -2,15 +2,33 @@ package scs.core.exception;
 
 import org.omg.CORBA.UserException;
 
+/**
+ * This exception is the most basic SCS exception and other exceptions derive
+ * from it.
+ * 
+ */
 public class SCSException extends Exception {
 
+  /**
+   * Default constructor.
+   */
   public SCSException() {
   }
 
+  /**
+   * Constructor that accepts an UserException as a cause.
+   * 
+   * @param cause The UserException object.
+   */
   public SCSException(UserException cause) {
     super(cause);
   }
 
+  /**
+   * Constructor that accepts a Throwable as a cause.
+   * 
+   * @param cause The Throwable object.
+   */
   public SCSException(Throwable cause) {
     super(cause);
   }
